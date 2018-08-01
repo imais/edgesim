d68fc5df40bba594eb6d3ce12f930f9bdd3fbafc
shigeru <Shigeru Imai>
2018-08-01 00:39:08 -0400

    Test mappings A, B, and C

--- src/clients.py
+++ src/clients.py
@@ -10,9 +10,6 @@ from scipy.stats import expon
 
 log = logging.getLogger()
 
-level_types = [['borough', 'cdp', 'city', 'municipality', 'other', 'town', 'township', 'village'], ['county'], ['state'], ['region']]
-L = len(level_types)
-
 
 class Comm:
 	@staticmethod
@@ -71,9 +68,9 @@ class Exec:
 
 	
 	@staticmethod
-	def estimate_query_time(dc_id, query_num):
+	def estimate_query_time(dc_id, queries_per_sec):
 		m = Exec.dc.loc[dc_id].m
-		return Exec.latency * (query_num / (Exec.theta1 + Exec.theta2 * m))
+		return Exec.latency * (queries_per_sec / (Exec.theta1 + Exec.theta2 * m))
 
 
 class QueryDestType(IntEnum):
@@ -100,18 +97,31 @@ class Query(object):
 		Query.query_resp_bytes = query_resp_bytes	
 		
 	
-	def __init__(self, src_lat, src_lng, dest_id):
+	def __init__(self, src_id, src_lat, src_lng, dest_id):
+		self.src_id = src_id
 		self.src_lat = src_lat
 		self.src_lng = src_lng
 		self.dest_id = dest_id
 
+	def query_to_str(self, dc, topo):
+		s = 'city=' + dc.loc[self.src_id, 'name'] + \
+			', (lat, lng)=('  + str(self.src_lat) + ', ' + str(self.src_lng) + '), ' + \
+			'dest=' + dc.loc[self.dest_id, 'name'] + \
+			', m=' + str(dc.loc[self.dest_id, 'm']) + \
+			', qps=' + str(QueryClient.queries_per_sec[self.dest_id]) + \
+			', total_resp time=' + str(self.total_resp_time * 1e3) + \
+			'ms (comm1=' + str(self.comm_req_time * 1e3) + \
+			', query=' + str(self.query_time * 1e3) + \
+			', comm2=' + str(self.comm_resp_time * 1e3) + ')'
+		return s
 
-	def estimate_resp_time(self, query_num):
+
+	def estimate_resp_time(self, queries_per_sec):
 		self.comm_req_time = Comm.estimate_query_comm_time(self.src_lat, self.src_lng, self.dest_id, Query.query_req_bytes)
 		if math.isnan(self.comm_req_time):
 			raise ValueError('self.comm_req_time is nan')
 		
-		self.query_time = Exec.estimate_query_time(self.dest_id, query_num)
+		self.query_time = Exec.estimate_query_time(self.dest_id, queries_per_sec)
 		if math.isnan(self.query_time):
 			raise ValueError('self.query_time is nan')
 		
@@ -122,62 +132,82 @@ class Query(object):
 		self.total_resp_time = self.comm_req_time + self.query_time + self.comm_resp_time
 
 
+class Hierarchy:
+	levels = [['borough', 'cdp', 'city', 'municipality', 'other', 'town', 'township', 'village'], ['county'], ['state'], ['region']]
+	L = len(levels)
+		
+
 class QueryClient(object):
-	num_queries = {}
+	queries = {}
+	queries_per_sec = {}
+	SQ_METER_TO_SQ_MI = 1.0 / (1e6 * (LatLngUtil.KM_PER_MILE**2))
+	
 
 	def create_queries(self):
 		self.queries = []
 
-		city_others_indices = self.dc.loc[self.dc.type.isin(level_types[0]) &
+		city_others_indices = self.dc.loc[self.dc.type.isin(Hierarchy.levels[0]) & \
+										  (self.dc.parent_id == self.city.parent_id) & \
 										  (self.dc.index != self.city.name)].index
 
 		county = self.dc.loc[self.city.parent_id]
-		county_others_indices = self.dc.loc[self.dc.type.isin(level_types[1]) &
+		county_others_indices = self.dc.loc[self.dc.type.isin(Hierarchy.levels[1]) & \
+											(self.dc.parent_id == county.parent_id) & \
 											(self.dc.index != county.name)].index
 		state =  self.dc.loc[county.parent_id]
-		state_others_indices = self.dc.loc[self.dc.type.isin(level_types[2]) &
+		state_others_indices = self.dc.loc[self.dc.type.isin(Hierarchy.levels[2]) & \
+										   (self.dc.parent_id == state.parent_id) & \
 										   (self.dc.index != state.name)].index
 
-		for i in range(self.num_queries_per_min):
+		test_queries = int(self.queries_per_min * self.conf['test_duration_sec'] / 60)
+		for i in range(test_queries):
 			r = random.uniform(0, 1)
 			dest_type = QueryDestType.CITY_SELF			
 			total = self.conf['query_dist'][int(dest_type)]
 			# query_dist: [city_self, city_others, county_self, county_others, state_self, state_others, region]
 			while total < r:
 				dest_type += 1
-				total += self.conf['query_dist'][int(dest_type)]				
+				total += self.conf['query_dist'][int(dest_type)]
 
-			if dest_type == QueryDestType.CITY_SELF:
-				dest_id = self.city.name
-			elif dest_type == QueryDestType.CITY_OTHER:
-				dest_id = random.choice(city_others_indices)
-			elif dest_type == QueryDestType.COUNTY_SELF:
-				dest_id = county.name
-			elif dest_type == QueryDestType.COUNTY_OTHER:
-				dest_id = random.choice(county_others_indices)
-			elif dest_type == QueryDestType.STATE_SELF:
-				dest_id = state.name
-			elif dest_type == QueryDestType.STATE_OTHER:
-				dest_id = random.choice(state_others_indices)
-			else:
-				# dest_type == QueryDestType.REGION
-				dest_id = self.dc.loc[state.parent_id].name
+			try:
+				dest_id = -1
+				if dest_type == QueryDestType.CITY_SELF:
+					dest_id = self.city.name
+				elif 0 < len(city_others_indices) and dest_type == QueryDestType.CITY_OTHER:
+					dest_id = random.choice(city_others_indices)
+				elif dest_type == QueryDestType.COUNTY_SELF:
+					dest_id = county.name
+				elif 0 < len(county_others_indices) and dest_type == QueryDestType.COUNTY_OTHER:
+					dest_id = random.choice(county_others_indices)
+				elif dest_type == QueryDestType.STATE_SELF:
+					dest_id = state.name
+				elif 0 < len(state_others_indices) and dest_type == QueryDestType.STATE_OTHER:
+					dest_id = random.choice(state_others_indices)
+				else:
+					# dest_type == QueryDestType.REGION
+					dest_id = self.dc.loc[state.parent_id].name
+			except IndexError:
+				print("dest_type={}, city={}".format(dest_type, self.city))
+				raise
 
 			(src_lat, src_lng) = LatLngUtil.get_rand_lat_lng(self.city.lat, self.city.lng,
-															 360, self.city_radius)
-			self.queries.append(Query(src_lat, src_lng, dest_id))
+															 360, self.city_radius_mi)
+			self.queries.append(Query(self.city.name, src_lat, src_lng, dest_id))
 			
-			if dest_id in QueryClient.num_queries:
-				QueryClient.num_queries[dest_id] += 1
+			if dest_id in QueryClient.queries:
+				QueryClient.queries[dest_id] += 1
 			else:
-				QueryClient.num_queries[dest_id] = 1				
+				QueryClient.queries[dest_id] = 1
+
+		for key, value in QueryClient.queries.iteritems():
+			QueryClient.queries_per_sec[key] = value / self.conf['test_duration_sec']
 
 
 	def estimate_query_resp_times(self):
-		print('Estimating query resp time from {}...'.format(self.city['name']))
+		# print('Estimating query resp time from {}...'.format(self.city['name']))
 		for query in self.queries:
-			query.estimate_resp_time(QueryClient.num_queries[query.dest_id])
-		return [q.total_resp_time for q in self.queries]
+			query.estimate_resp_time(QueryClient.queries_per_sec[query.dest_id])
+		return self.queries
 			
 					 
 	def __init__(self, conf, dc, topo, city):
@@ -185,10 +215,10 @@ class QueryClient(object):
 		self.dc = dc
 		self.topo = topo
 		self.city = city
-		self.city_radius = math.sqrt(city.land_area / math.pi)
-		self.num_queries_per_min = int(city.population * self.conf['ratio_of_adults'] * \
+		self.city_radius_mi = math.sqrt(city.land_area * QueryClient.SQ_METER_TO_SQ_MI / math.pi)
+		self.queries_per_min = int(city.population * self.conf['ratio_of_adults'] * \
 									   self.conf['ratio_of_adult_smartphone_owners'] * \
-									   self.conf['num_queries_per_smartphone_per_hour'] / 60)
+									   self.conf['queries_per_smartphone_per_hour'] / 60)
 		self.create_queries()
 
 
@@ -201,28 +231,42 @@ class DataAggregator:
 
 
 	@staticmethod
-	def estimate_aggr_time():
+	def aggregate():
 		conf = DataAggregator.conf
 		dc = DataAggregator.dc
 		topo = DataAggregator.topo
 		data_out = conf['bytes_reduce_out']
 
-		max_aggr_times = []
-		for l in range(L):
-			aggr_times = []
-			entities = topo.loc[topo.type.isin(level_types[l])]
+		max_aggr_results = []
+		for l in range(Hierarchy.L):
+			aggr_results = []
+			entities = topo.loc[topo.type.isin(Hierarchy.levels[l])]
 			for index, entity in entities.iterrows():
 				if l == 0:
 					topo.loc[index, 'data_in'] = dc.loc[index].population * conf['sensors_per_person'] * conf['bytes_per_sensor_per_time_window']
 				dc1_id = entity.dc_id					
-				if l < L - 1:
+				if l < Hierarchy.L - 1:
 					topo.loc[entity.parent_id, 'data_in'] += data_out
 					dc2_id = topo.loc[entity.parent_id].dc_id
 					aggr_time = Exec.estimate_map_reduce_time(dc1_id, topo.loc[index, 'data_in']) + Comm.estimate_dc_comm_time(dc1_id, dc2_id, data_out)
 				else:
+					dc2_id = None
 					aggr_time = Exec.estimate_map_reduce_time(dc1_id, topo.loc[index, 'data_in'])
-				aggr_times.append(aggr_time)
-			# print aggr_times
-			max_aggr_times.append(max(aggr_times))
-		# print total_aggr_time
-		return max_aggr_times
+				result = (aggr_time, dc1_id, dc2_id, topo.loc[index, 'data_in'])
+				aggr_results.append(result)
+					
+			max_aggr_results.append(max(aggr_results, key=lambda (a, b, c, d): a))
+
+		return max_aggr_results
+
+
+	@staticmethod	
+	def result_to_str(res):
+		dc = DataAggregator.dc
+		s = str(res[0]) + 'ms, dc1=' + dc.loc[res[1], 'name'] + "/" + dc.loc[res[1], 'state_code']
+		if res[2] is not None:
+			s += ', dc2=' + dc.loc[res[2], 'name'] + "/" + dc.loc[res[2], 'state_code']
+		s += ', data_in=' + str(res[3]) + 'bytes'
+		return s
+		
+		
--- src/sim.py
+++ src/sim.py
@@ -2,15 +2,18 @@ import argparse
 import json
 import logging
 import logging.config
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-from clients import Comm, Exec, Query, QueryClient, DataAggregator
+import sys
+from clients import Comm, Exec, Query, QueryClient, DataAggregator, Hierarchy
 
 log = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-DC_FILE = "./data/test-dc.tsv"
+# DC_FILE = "./data/test-dc.tsv"
+DC_FILE = "./data/us-northeast.tsv"
 
 
 def parse_args():
@@ -43,10 +46,14 @@ def load_data(mapping):
 		topo.dc_id = dc.index
 	elif mapping == 'b':
 		# Use state and region DCs
+		prev_state = None
 		for i, row in topo.iterrows():
-			if row.type == 'city':
+			if dc.loc[i].state_code != prev_state:
+				print('Loading data for {}...'.format(dc.loc[i].state_code))
+				prev_state = dc.loc[i].state_code
+			if row.type in Hierarchy.levels[0]: # city, town, village, ...
 				topo['dc_id'][i] = dc.loc[dc.loc[i].parent_id].parent_id
-			elif row.type == 'county':
+			elif row.type in Hierarchy.levels[1]:
 				topo['dc_id'][i] = dc.loc[i].parent_id
 			else:
 				# state or region
@@ -69,29 +76,58 @@ def init(args):
 	Query.set_params(conf['query_req_bytes'], conf['query_resp_bytes'])
 	DataAggregator.set_params(conf, dc, topo)
 
-	city_ids = dc.loc[dc.type == 'city'].index
-	query_clients = []
-	for city_id in city_ids:
-		print('Creating query client for {}...'.format(dc.loc[city_id]['name']))
-		query_clients.append(QueryClient(conf, dc, topo, dc.loc[city_id]))
-
-	return conf, dc, topo, query_clients
+	return conf, dc, topo
 	
 
 def main(args):
 	global dc, topo, resp_times
-	conf, dc, topo, query_clients = init(args)
+	conf, dc, topo = init(args)
 	log.info('Configs: {}'.format(conf))
 
-	max_aggr_times = DataAggregator.estimate_aggr_time()
-	
-	resp_times = []
-	for query_client in query_clients:
-		resp_times += query_client.estimate_query_resp_times()
+	# Estimate data aggregation time
+	aggr_results = DataAggregator.aggregate()	
 
-	print("Total Aggregation Time: {}ms (lv1={}ms, lv2={}ms, lv3={}ms, lv4={}ms)".format(np.sum(max_aggr_times) * 1000, max_aggr_times[0] * 1000, max_aggr_times[1] * 1000, max_aggr_times[2] * 1000, max_aggr_times[3] * 1000))		
-	print("Response Time: max={}ms, avg={}ms, min={}ms".
-		  format(np.max(resp_times)*1000, np.mean(resp_times)*1000, np.min(resp_times)*1000))
+	# Estimate query response times
+	city_ids = dc.loc[dc.type.isin(Hierarchy.levels[0])].index
+	query_clients = []
+	prev_state = None
+	for city_id in city_ids:
+		if dc.loc[city_id].state_code != prev_state:
+			print('Creating query clients for {}...'.format(dc.loc[city_id].state_code))
+			prev_state = dc.loc[city_id].state_code
+		query_clients.append(QueryClient(conf, dc, topo, dc.loc[city_id]))	
+
+	query_results = []
+	prev_state = None
+	for query_client in query_clients:
+		if dc.loc[query_client.city.name].state_code != prev_state:
+			print('Estimating query resp time for {}...'.format(dc.loc[query_client.city.name].state_code))
+			prev_state = dc.loc[query_client.city.name].state_code
+		query_results += query_client.estimate_query_resp_times()
+
+	# Display results
+	print
+	total_aggr_time = sum(result[0] for result in aggr_results)
+	print("Total Aggregation Time: {}ms".format(total_aggr_time))
+	for l in range(Hierarchy.L):
+		print("L{}: {}".format(l+1, DataAggregator.result_to_str(aggr_results[l])))
+
+	print
+	print("Response Time:")
+	max_query = max(query_results, key=lambda q: q.total_resp_time)
+	min_query = min(query_results, key=lambda q: q.total_resp_time)
+	print("Max: {}".format(max_query.query_to_str(dc, topo)))
+	print("Min: {}".format(min_query.query_to_str(dc, topo)))
+	resp_times_ms = [q.total_resp_time * 1000 for q in query_results]	
+	print("Avg: {}ms".format(np.mean(resp_times_ms)))
+
+	num_bins = 40
+	counts, bin_edges = np.histogram(resp_times_ms, bins=num_bins, normed=True)
+	cdf = np.cumsum(counts)
+	plt.plot (bin_edges[1:], cdf/cdf[-1])
+	print bin_edges[1:]
+	print cdf/cdf[-1]
+	plt.show()		  
 		
 	
 if __name__ == '__main__':
